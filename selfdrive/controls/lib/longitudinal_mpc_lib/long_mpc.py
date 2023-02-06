ui: make experimental mode longitudinal more clear #27381 traffic sign... ui freezing fix.. update comma~ Hyundai: detect FCA feature (#27322) full-mpc-ui 구간별 Accel 설정..... 5->6구간... 횡컨 not longActive일때 xState => cruise상태로 변경.. gasPressed라면 신호정지모드로 안가도록 fix.. trafficState... fix.. Test: panda에서  str_req 를 강제 1로 한것... torque_fauilt를 car_controller로 강제 1로하도록함... decelCruise 속도셋 fix... 크루즈 갭버튼모드3번: 한번더누르면 크루즈 RESUME.. 안전벨트 디스인게이지 삭제... safety : steerReq 원복.. 신호감지: 미리속도 줄이기(감지정확도 향상?) 시험용 ui params  1초마다 업뎃~ nda속도제한 시작/종료시점 설정 속도별 가속도설정.. fix 속도별가속도 fix..
속도별 가속도 설정~

우측깜박이시 브레이크해제크루즈ON: 사용중지~

comma update~ 230208

브레이크해제:전방차량:기존속도

roadEdge는 바로앞의 것으로~

크루즈셋 최소스피드~

apilotman: 드라이브모드..

selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -5,6 +5,7 @@ import numpy as np
 from common.realtime import sec_since_boot
 from common.numpy_fast import clip, interp
 from system.swaglog import cloudlog
+# WARNING: imports outside of constants will not trigger a rebuild
 from selfdrive.modeld.constants import index_function
 from selfdrive.controls.lib.radar_helpers import _LEAD_ACCEL_TAU
 from common.conversions import Conversions as CV
@@ -497,8 +498,8 @@ class LongitudinalMpc:
         self.startSignCount = self.startSignCount + 1 if startSign else 0
         self.stopSignCount = self.stopSignCount + 1 if stopSign else 0
 
-        startSign = 1 if self.startSignCount > 0.3 * DT_MDL else 0
-        stopSign = 1 if self.stopSignCount > 0.3 * DT_MDL else 0
+        # trafficState: 2:StartSign, 1:StopSign, 0: Ignore
+        self.trafficState = 1 if self.stopSignCount * DT_MDL > 0.3 else 2 if self.startSignCount * DT_MDL > 0.3 else 0
 
         if self.xState == XState.e2eStop: # and abs(self.xStop - model_x) < 20.0:
           stopFilterX = self.xStopFilter.process(model_x, median = True)  # -v_ego는 longitudinalPlan에서 v_ego만큼 더해서 나옴.. 마지막에 급감속하는 문제가 발생..
@@ -510,7 +511,6 @@ class LongitudinalMpc:
           
         model_x = self.xStop
 
-        self.trafficState = 1 if self.stopSignCount*DT_MDL > 0.3 else 2 if self.startSignCount*DT_MDL > 0.3 else 0
         if self.e2ePaused:
           self.trafficState += 100  # 이렇게하면.... 이벤트발생이 안됨...
 
@@ -530,7 +530,7 @@ class LongitudinalMpc:
             v_cruise = 0.0
           if radarstate.leadOne.status and (radarstate.leadOne.dRel - model_x) < 2.0:
             self.xState = XState.lead
-          elif startSign == 1:  # 출발신호
+          elif self.trafficState == 2:  # 출발신호
             self.xState = XState.e2eCruise
             self.e2ePaused = True #출발신호가 나오면 이때부터 신호무시하자... 출발후 정지하는 경우가 생김..
           if carstate.gasPressed: # or cruiseButtonCounterDiff>0:       #예외: 정지중 accel을 밟으면 강제주행모드로 변경
@@ -543,13 +543,13 @@ class LongitudinalMpc:
             self.xState = XState.e2eCruise
             self.e2ePaused = True
           if cruiseButtonCounterDiff > 0:
-            self.xState = XState.e2eStop if stopSign == 1 else XState.e2eCruise
+            self.xState = XState.e2eStop if self.trafficState == 1 else XState.e2eCruise
             self.e2ePaused = False
         #E2E_CRUISE: 주행상태.
         else:
           if self.status:
             self.xState = XState.lead
-          elif stopSign == 1 and not self.e2ePaused:                 #신호인식이 되면 정지모드
+          elif self.trafficState == 1 and not self.e2ePaused and not carstate.gasPressed:                 #신호인식이 되면 정지모드
             self.buttonStopDist = 0
             self.xState = XState.e2eStop
           else:
@@ -561,8 +561,9 @@ class LongitudinalMpc:
         self.trafficState = 0
 
       fakeCruiseDistance = 0.0
+
       #3단계: 조건에 따른. 감속및 주행.
-      if self.xState in [XState.lead, XState.cruise] or self.e2ePaused:
+      if self.xState in [XState.lead, XState.cruise] or self.e2ePaused or controls.longActiveUser<=0:
         model_x = 1000.0
       elif self.xState == XState.e2eCruise:
         if carstate.gasPressed:
@@ -589,7 +590,13 @@ class LongitudinalMpc:
       self.comfort_brake *= mySafeModeFactor
       self.longActiveUser = controls.longActiveUser
       self.cruiseButtonCounter = controls.cruiseButtonCounter
-      x2 = model_x * np.ones(N+1) + self.trafficStopDistanceAdjust
+
+      stop_x = model_x
+      # 급격히 정지하는 걸 막아보자~ 시험.
+      if self.xState == XState.e2eStop and model_x < 3.0: # 신호정지이고 3M이내이면.. 급정거... 최소거리확보해야할... test
+        stop_x = max(v_ego * v_ego / (1.0 * 2), model_x)  # -1 m/s^2으로 감속할때 정지거리..
+
+      x2 = stop_x * np.ones(N+1) + self.trafficStopDistanceAdjust
 
       # Fake an obstacle for cruise, this ensures smooth acceleration to set speed
       # when the leads are no factor.